bc18a0c13cb8457b3f5c7298812fe72fe147b76c
Renato Groffe <renato.groffe@yahoo.com.br>
2025-02-23 22:32:19 -0300

    Ajustes

--- src/appgroffe/lib/main.dart
+++ src/appgroffe/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'App do Groffe Home Page'),
+      home: const MyHomePage(title: 'App do Renato Groffe Home Page'),
     );
   }
 }
